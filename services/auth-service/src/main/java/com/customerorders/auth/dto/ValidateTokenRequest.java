de7fa604451c9ccb00d0fa571d665368197ac943
Tarek Fouda <tareksfouda@gmail.com>
2026-09-09 00:31:30 -0400

    Add ValidateTokenRequest DTO for Auth Service

---
+++ services/auth-service/src/main/java/com/customerorders/auth/dto/ValidateTokenRequest.java
@@ -0,0 +1,12 @@
+package com.customerorders.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateTokenRequest {
+    private String token;
+}
